81bf4cbe46a30fcf73576cb45ce10dce9df69491
vcslav-v <donn.nus@gmail.com>
2024-01-05 13:55:23 +0100

    0.2.1

--- twine_to_json/__init__.py
+++ twine_to_json/__init__.py
@@ -3,7 +3,7 @@ from twine_to_json.schemas import Result
 from twine_to_json.main import parse
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 __author__ = 'Vaclav_V'
 __all__ = ['parse', 'Result']
 
