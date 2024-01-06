d26a3c9d1eda30dd480d09486838e52c56ffc24e
vcslav-v <donn.nus@gmail.com>
2024-01-06 13:38:51 +0100

    0.3.7

--- twine_to_json/__init__.py
+++ twine_to_json/__init__.py
@@ -3,6 +3,6 @@ from twine_to_json.schemas import Result
 from twine_to_json.main import convert
 
 
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 __author__ = 'Vaclav_V'
 __all__ = ['convert', 'Result']
